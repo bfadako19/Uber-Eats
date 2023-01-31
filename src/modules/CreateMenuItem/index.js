6c99ac3882bb48d4a5cbd2cf7f3ee43306c8fdc2
Bryce Fadako <fadakob@chc.edu>
2023-01-31 10:38:30 -0500

    1/31/23

--- src/modules/CreateMenuItem/index.js
+++ src/modules/CreateMenuItem/index.js
@@ -1,6 +1,54 @@
+import { Card, Input, Button, InputNumber, message,Form } from "antd";
+const {TextArea} = Input;
+
 const CreateMenuItem = () => {
-    return(
-        <div> Create Menu Item</div>
+    const onFinish = ({name,description,price}) => {
+        if(!name){
+            message.error('Name Required!');
+            return;
+        }
+        if(!description){
+            message.error('Description Required!');
+            return;
+        }
+        if(!price){
+            message.error('Price Required!');
+            return;
+        }
+        message.success('Menu Item Created!')
+
+    };
+    return (
+        <Card title={'Create New Item'} style={styles.page}>
+            <Form layout="vertical" onFinish={onFinish}>
+                <Form.Item label = {'Name'} required name='name'>
+                    <Input placeholder="Enter Name"/>
+                </Form.Item>
+                <Form.Item label={'Description'} required name='description'>
+                    <TextArea 
+                    rows = {4}
+                    placeholder='Enter Description'
+                    />
+                </Form.Item>
+                <Form.Item label={'Price'} required name = 'price'>
+                    <InputNumber placeholder="Enter Price" />
+                </Form.Item>
+                <Form.Item label={'Image'} name='image'>
+                    <Input placeholder="Enter Image Link"/>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">Submit</Button>
+                </Form.Item>
+                
+            </Form>
+        </Card>
     );
+};
+
+const styles = {
+    page: {
+        margin: 20,
+    },
 }
+
 export default CreateMenuItem;
--- src/modules/CreateRestaurant/index.js
+++ src/modules/CreateRestaurant/index.js
@@ -1,17 +1,48 @@
-import {Card, Input, Button, InputNumber, message, TextArea, Form} from 'antd';
+import {Card, Input, Button, message, Form} from 'antd';
+
+const onFinish = ({name,address,image}) => {
+    if(!name){
+        message.error('Name Required!');
+        return;
+    }
+    if(!address){
+        message.error('Address Required!');
+        return;
+    }
+    if(!image){
+        message.error('Image Required!');
+        return;
+    }
+    message.success('Restaurant Created!')
+
+};
 const CreateRestaurant = () => {
-    const styles ={
-        page:{
-            margin: 20,
-        },
-}
-    return(
-        <Card title={'Create New Item'} style={styles.page}>
-            <Form layout="vertical">
-
+    return (
+        <Card title={'Restaurant Details'} style={styles.page}>
+            <Form layout="vertical" onFinish={onFinish}>
+                <Form.Item label = {'Name'} required name='name'>
+                    <Input placeholder="Enter Name"/>
+                </Form.Item>
+                <Form.Item label={'Address'} required name='address'>
+                    <Input
+                    placeholder='Enter Address'
+                    />
+                </Form.Item>
+                <Form.Item label={'Image'} required name='image'>
+                    <Input placeholder="Enter Image Link"/>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">Submit</Button>
+                </Form.Item>
+                
             </Form>
-
         </Card>
     );
+};
+
+const styles = {
+    page: {
+        margin: 20,
+    },
 }
 export default CreateRestaurant;
